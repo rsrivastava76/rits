b20a28338c20042b719c34ee338d7dd98bc3734d
Ritesh Srivastava <rsrivastava76@hotmail.com>
2017-04-20 22:32:41 +0530

    moving modules as individuals Repo

--- Crawler/src/crawler/persistence/Persister.java
+++
@@ -1,8 +0,0 @@
-package crawler.persistence;
-
-import crawler.datamodel.CrawlerDataModel;
-
-public interface Persister {
-
-	public void persist(CrawlerDataModel crawlerDataModel);
-}
--- Crawler/src/crawler/query/Query.java
+++
@@ -1,8 +0,0 @@
-package crawler.query;
-
-import java.util.List;
-
-public interface Query {
-
-	public List<String> queryUrlsByContent(String content);
-}
