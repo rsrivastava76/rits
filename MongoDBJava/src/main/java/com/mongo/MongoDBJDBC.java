14dd7a11a67e3eab4fd310173b39a94655a7aa7e
Ritesh Srivastava <rsrivastava76@hotmail.com>
2017-04-20 21:07:31 +0530

    cleanup and query updates

--- MongoDBJava/src/main/java/com/mongo/MongoDBJDBC.java
+++ MongoDBJava/src/main/java/com/mongo/MongoDBJDBC.java
@@ -1,21 +1,22 @@
 package com.mongo;
 
+import static com.mongodb.client.model.Filters.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import org.bson.BSONEncoder;
-import org.bson.BasicBSONEncoder;
 import org.bson.Document;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBCursor;
-import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+
+import static com.mongodb.client.model.Sorts.descending;
+import static com.mongodb.client.model.Projections.excludeId;
 
 public class MongoDBJDBC {
 
@@ -26,101 +27,81 @@ public class MongoDBJDBC {
 			String user = "mongouser"; // the user name
 			String password = "pass@123"; // the password as a character array
 			String database = "extended_doc"; // the name of the database in which the user is defined
+			String server1 = "test.mongodb.firstrain.com";
+			int port = 27017;
 
 			List<ServerAddress> seeds = new ArrayList<ServerAddress>();
-			seeds.add( new ServerAddress( "test.mongodb.firstrain.com",27017 ));
+			seeds.add( new ServerAddress( server1,port ));
 
 			List<MongoCredential> credentials = new ArrayList<MongoCredential>();
-			credentials.add(
-					MongoCredential.createScramSha1Credential(
-							user,
-							database,
-							password.toCharArray()
-							)
-					);
-			MongoClient mongoClient = new MongoClient( seeds, credentials );
+			credentials.add( MongoCredential.createScramSha1Credential( user, database, password.toCharArray() ) );
 
+			MongoClient mongoClient = new MongoClient( seeds, credentials );
 			MongoDatabase db = mongoClient.getDatabase(database);
 
 			System.out.println("Connect to DataBase Sucessfully");
 
 			MongoCollection<Document> docs = db.getCollection("meta");
 
-			System.out.println("docs == " + docs.count() );
+			System.out.println("Total Records in Meta Collections == " + docs.count() );
 
 			Document myDoc = docs.find().first();
-			//System.out.println(myDoc.toJson());
+			System.out.println(" First Record in Collections " + myDoc.toJson());
+			System.out.println("******************************11111111***************************************");
 
-		//	{ "_id" : { "$oid" : "58e720899b629f6a6dcf16c0" }, "itemsName" : "Orange", "soldQty" : 4.0 }
-			
 			// now use a query to get 1 document out
-		//	myDoc = docs.find(eq("ng.ngrams.phrase", "supervisors chambers")).first();
-			//System.out.println("Test ==" + myDoc.toJson());
+			myDoc = docs.find(eq("ng.ngrams.phrase", "supervisors chambers")).first();
+			System.out.println("First Record in Collections filter on Phrase 'supervisors chambers' ==" + myDoc.toJson());
 
 			//db.meta.find({"ng.ngrams":{$elemMatch:{"phrase":"trump","firstLocation":"Q1"}}}).limit(1)
-			
+
+			// element search 
 			Document  statusQuery = new Document("phrase", "trump").append("firstLocation", "Q1");
-	        
 			Document elemMatchQuery = new Document("$elemMatch", statusQuery);
 
 			Document fields = new Document();
-	        
-	        fields.put("ng.ngrams", elemMatchQuery);
-	        
-	      //  fields.put("name", 1);
-	        //fields.put("config", 1);
+			fields.put("ng.ngrams", elemMatchQuery);
 
-	        
-	        System.out.println("fikter Q count == " + docs.count(fields));
+			System.out.println("Filter Records Count == " + docs.count(fields));
 
-	        FindIterable<Document> cursor = docs.find(fields).limit(2);
-	        //int i = 0;
-	        
+			// Find top 2 results of filter records
+			FindIterable<Document> cursor = docs.find(fields).limit(2);
+			//int i = 0;
+
 			for (Document document : cursor) {
-				System.out.println(document.toJson());
+				System.out.println("Documents = " + document.toJson());
 			}
-	        
-			//MongoCursor<Document> cursor = docs.find().iterator();
-/*
+
+			System.out.println("******************************22222222***************************************");
+
 			// now use a range query to get a larger subset
-			cursor = docs.find(gt("i", 50)).iterator();
+			MongoCursor<Document> mCursor  = docs.find(gt("imageScore", 1)).iterator();
 
 			try {
-				while (cursor.hasNext()) {
-					System.out.println(cursor.next().toJson());
+				while (mCursor.hasNext()) {
+					System.out.println(mCursor.next().toJson());
 				}
 			} finally {
-				cursor.close();
-			}
-*/
-			// range query with multiple constraints
-//			cursor = docs.find(and(eq("ng.ngrams.phrase", "trump"), eq("ng.ngrams.firstLocation", "Q1"))).iterator();
-
-/*			try {
-				while (cursor.hasNext()) {
-					System.out.println(cursor.next().toJson());
-				}
-			} finally {
-				cursor.close();
+				mCursor.close();
 			}
 
-			// Query Filters
-			myDoc = docs.find(eq("i", 71)).first();
-			System.out.println(myDoc.toJson());
+			System.out.println("******************************33333333***************************************");
 
 			// Sorting
-			myDoc = docs.find(exists("i")).sort(descending("i")).first();
+			myDoc = docs.find(exists("docImage")).sort(descending("imageScore")).first();
 			System.out.println(myDoc.toJson());
+
+			System.out.println("******************************44444444***************************************");
 
 			// Projection
 			myDoc = docs.find().projection(excludeId()).first();
 			System.out.println(myDoc.toJson());
-*/
+
+			System.out.println("******************************55555555***************************************");
 
 			mongoClient.close();
 
 		} catch (Exception e) {
-			// TODO: handle exception
 			e.printStackTrace();
 		}
 
